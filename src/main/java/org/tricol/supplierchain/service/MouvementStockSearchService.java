bb20a73ff0faa994cfd93238c81f749cdaa9b244
lahcen <lahcenwirghanec11@gmail.com>
2025-11-14 16:12:16 +0100

    search functionality

---
+++ src/main/java/org/tricol/supplierchain/service/MouvementStockSearchService.java
@@ -0,0 +1,37 @@
+package org.tricol.supplierchain.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.tricol.supplierchain.dto.request.MouvementStockSearchCriteria;
+import org.tricol.supplierchain.dto.response.MouvementStockResponseDTO;
+import org.tricol.supplierchain.entity.MouvementStock;
+import org.tricol.supplierchain.enums.TypeMouvement;
+import org.tricol.supplierchain.mapper.MouvementStockMapper;
+import org.tricol.supplierchain.repository.MouvementStockRepository;
+import org.tricol.supplierchain.specification.MouvementStockSpecifications;
+
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MouvementStockSearchService {
+    
+    private final MouvementStockRepository repository;
+    private final MouvementStockMapper mapper;
+    
+    public Page<MouvementStockResponseDTO> searchMouvements(
+            MouvementStockSearchCriteria criteria, 
+            Pageable pageable) {
+        
+        Specification<MouvementStock> spec = MouvementStockSpecifications.withCriteria(criteria);
+        Page<MouvementStock> mouvements = repository.findAll(spec, pageable);
+        return mouvements.map(mapper::toResponseDTO);
+    }
+    
+
+}
