d52d7cf044248ec60180d66d55b3d52ee0b21a15
lahcen <lahcenwirghanec11@gmail.com>
2025-12-19 20:43:13 +0100

    Implement Custom UserDetails and jwt service

---
+++ src/main/java/org/tricol/supplierchain/security/CustomUserDetailsService.java
@@ -0,0 +1,59 @@
+package org.tricol.supplierchain.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.tricol.supplierchain.entity.UserApp;
+import org.tricol.supplierchain.repository.UserRepository;
+import org.tricol.supplierchain.service.inter.PermissionService;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final PermissionService permissionService;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserApp user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        if (!user.getEnabled()) {
+            throw new UsernameNotFoundException("User Account is disabled: ");
+        }
+
+        return User
+                .builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .authorities(getAuthorities(user))
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(!user.getEnabled())
+                .build();
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(UserApp user) {
+        Set<String> permissions = permissionService.getUserPermissions(user);
+        Set<GrantedAuthority> authorities = permissions.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toSet());
+        if (user.getRole() != null) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+        }
+        return authorities;
+    }
+}
